fdf9ea9a57de4817ec9c6525edd01e63a30a874e
sunita0987 <sunita0987546@gmail.com>
2025-02-13 14:17:06 +0530

    add 2 section

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,3 +2,21 @@
 document.getElementById("menu-toggle").addEventListener("click", function () {
     document.getElementById("nav-links").classList.toggle("show");
 });
+//slider hero section
+
+let currentIndex = 0;
+
+function moveSlide(direction) {
+    const slides = document.querySelector('.slides');
+    const totalSlides = document.querySelectorAll('.slide').length;
+
+    currentIndex += direction;
+
+    if (currentIndex < 0) {
+        currentIndex = totalSlides - 1;
+    } else if (currentIndex >= totalSlides) {
+        currentIndex = 0;
+    }
+
+    slides.style.transform = `translateX(-${currentIndex * 100}%)`;
+}
